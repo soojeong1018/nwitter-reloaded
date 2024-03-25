64d967a1d09a0479e5644eeab392a9714da16deb
ksj <tnwjd940412@gmail.com>
2024-03-25 23:07:53 +0900

    03/25 firebase start

--- src/App.tsx
+++ src/App.tsx
@@ -2,27 +2,62 @@ import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import Layout from "./components/layout.tsx";
 import Home from "./routes/home.tsx";
 import Profile from "./routes/profile.tsx";
+import Login from "./routes/login.tsx";
+import CreateAccount from "./routes/create-account.tsx";
+import {createGlobalStyle} from "styled-components";
+import reset from "styled-reset";
+import {useEffect, useState} from "react";
+import LoadingScreen from "./components/loading-screen.tsx";
 
-function App() {
-  const router = createBrowserRouter([{
-    path: "/",
-    element: <Layout />,
-    children: [
-      {
-        path:"",
-        element: <Home />
-      },
-      {
-        path:"profile",
-        element: <Profile />
-      },
+const router = createBrowserRouter([{
+  path: "/",
+  element: <Layout />,
+  children: [
+    {
+      path:"",
+      element: <Home />
+    },
+    {
+      path:"profile",
+      element: <Profile />
+    },
 
-    ]
+  ]
+},
+  {
+    path: "/login",
+    element: <Login />
+  },
+  {
+    path: "/create-account",
+    element: <CreateAccount />
   }]);
+
+const GlobalStyles = createGlobalStyle`
+  ${reset};
+  * {
+    box-sizing: border-box;
+  }
+  body {
+    background-color: black;
+    color: white;
+    font-family: 'system-ui', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+  `;
+function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  const init = async () => {
+    // wait for firebase
+    setIsLoading(false);
+  };
+  useEffect(() => {
+    init();
+  }, []);
 
   return (
     <>
-      <RouterProvider router={router} ></RouterProvider>
+      <GlobalStyles />
+      {isLoading ? <LoadingScreen/> : <RouterProvider router={router} /> }
     </>
   )
 }
---
+++ src/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyA5Cy7Xt9Drs4w8g8-h1RzshidDbmtffUo",
+    authDomain: "nwitter-reloaded-4df31.firebaseapp.com",
+    projectId: "nwitter-reloaded-4df31",
+    storageBucket: "nwitter-reloaded-4df31.appspot.com",
+    messagingSenderId: "462157437545",
+    appId: "1:462157437545:web:88775937711e061c0ee0f8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
